6e67332ea43a0ea54bf2e352a3d6a6f396261338
yasukou1202 <yasukou1202@gmail.com>
2026-06-01 21:22:43 +0900

    fix chat layout absolute positioning

--- js/app.js
+++ js/app.js
@@ -48,10 +48,7 @@ function goPage(id, btn) {
     loadSneakers();
   }
   if (id === 'chat' && typeof goChat === 'function') {
-    document.getElementById('mainScroll').style.overflow = 'hidden';
     goChat();
-  } else {
-    document.getElementById('mainScroll').style.overflow = '';
   }
 }
 
