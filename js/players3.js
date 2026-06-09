434e19dc382cdf039026e69017452cbf6fcbabfd
yasukou1202 <yasukou1202@gmail.com>
2026-06-09 19:26:12 +0900

    use Date.now() for data.json cache busting

--- js/players3.js
+++ js/players3.js
@@ -117,7 +117,7 @@ async function loadPlayersFromAPI() {
 
   try {
     // ESPN スタッツリーダーAPIから得点順で選手を取得
-    const url = `https://courtside-jp.github.io/mentality/data.json?v=1781000713752292000`;
+    const url = `https://courtside-jp.github.io/mentality/data.json?v=${Date.now()}`;
     const res = await fetchWithTimeout(url, {}, 8000);
     if (!res.ok) throw new Error('ESPN Error ' + res.status);
     const data = await res.json();
